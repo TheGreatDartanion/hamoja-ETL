cecfb202d8e13ac080a45727bad604cdf991cfe1
Williams, Dartanion De Angelo <dartanion@dartanion.net>
2019-12-15 15:13:52 -0500

    Updating code

---
+++ config.py
@@ -0,0 +1,23 @@
+'''
+Here is my MySQL DB information.
+
+You can open MySQL workbench and login with the host name below.
+
+Feel free to use my user name and password (see below)
+
+Once you are connected, open the travel_auth database 
+(USE travel_auth;)
+
+Then query the client_info table 
+(SELECT * FROM travel_info)
+
+This should be enough to get you started.
+
+Good luck!
+
+'''
+remote_db_endpoint = 'databootcamp.cze9dquabwlh.us-east-2.rds.amazonaws.com'
+remote_db_port = 3306
+remote_gwsis_dbuser = 'admin'
+remote_gwsis_dbpwd = 'Dartlings!'
+remote_gwsis_dbname = 'travel_auth'
